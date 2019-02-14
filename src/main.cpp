eb219d5b1003f3922df2defb19e26d3c92722711
Romeu Vieira <romeu.bizz@gmail.com>
2019-02-14 09:25:09 +0000

    Change error buffer var to be local instead of global

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,6 @@
 
 #include <curl/curl.h>
 
-static char errorBuffer[CURL_ERROR_SIZE];
-
 static int writer(char *data, size_t size, size_t nmemb, std::string *writer_data)
 {
 	if(writer_data == nullptr) {
@@ -18,6 +16,7 @@ static int writer(char *data, size_t size, size_t nmemb, std::string *writer_dat
 static bool init(CURL *&conn, char *url, std::string &buffer)
 {
 	CURLcode code;
+	char curl_error_buffer[CURL_ERROR_SIZE];
 
 	conn = curl_easy_init();
 
@@ -26,7 +25,7 @@ static bool init(CURL *&conn, char *url, std::string &buffer)
 		return false;
 	}
 
-	code = curl_easy_setopt(conn, CURLOPT_ERRORBUFFER, errorBuffer);
+	code = curl_easy_setopt(conn, CURLOPT_ERRORBUFFER, curl_error_buffer);
 	if (code != CURLE_OK) {
 		std::printf("Failed to set error buffer [%d]\n", code);
 		return false;
@@ -34,25 +33,25 @@ static bool init(CURL *&conn, char *url, std::string &buffer)
 
 	code = curl_easy_setopt(conn, CURLOPT_URL, url);
 	if (code != CURLE_OK) {
-		std::printf("Failed to set URL [%s]\n", errorBuffer);
+		std::printf("Failed to set URL [%s]\n", curl_error_buffer);
 		return false;
 	}
 
 	code = curl_easy_setopt(conn, CURLOPT_FOLLOWLOCATION, 1L);
 	if (code != CURLE_OK) {
-		std::printf("Failed to set redirect option [%s]\n", errorBuffer);
+		std::printf("Failed to set redirect option [%s]\n", curl_error_buffer);
 		return false;
 	}
 
 	code = curl_easy_setopt(conn, CURLOPT_WRITEFUNCTION, writer);
 	if (code != CURLE_OK) {
-		std::printf("Failed to set writer [%s]\n", errorBuffer);
+		std::printf("Failed to set writer [%s]\n", curl_error_buffer);
 		return false;
 	}
 
 	code = curl_easy_setopt(conn, CURLOPT_WRITEDATA, &buffer);
 	if (code != CURLE_OK) {
-		std::printf("Failed to set write data [%s]\n", errorBuffer);
+		std::printf("Failed to set write data [%s]\n", curl_error_buffer);
 		return false;
 	}
 
@@ -63,7 +62,9 @@ int main(int argc, char **argv)
 {
 	CURL *conn = nullptr;
 	CURLcode code;
+
 	std::string buffer;
+	char curl_error_buffer[CURL_ERROR_SIZE];
 
 	if (argc != 2) {
 		std::printf("Usage: %s <url>\n", argv[0]);
@@ -81,7 +82,7 @@ int main(int argc, char **argv)
 	curl_easy_cleanup(conn);
 
 	if (code != CURLE_OK) {
-		std::printf("Failed to get '%s' [%s]\n", argv[1], errorBuffer);
+		std::printf("Failed to get '%s' [%s]\n", argv[1], curl_error_buffer);
 		exit(EXIT_FAILURE);
 	}
 
