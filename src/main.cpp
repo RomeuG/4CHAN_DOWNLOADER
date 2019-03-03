0da8b5fcbe82d7b74fd6ff93a049482ba00a38f5
Romeu Vieira <romeu.bizz@gmail.com>
2019-03-03 17:14:47 +0000

    Code to download images from url.

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,64 @@
 #include <libxml2/libxml/HTMLparser.h>
 #include <curl/curl.h>
 
-static int writer(char *data, size_t size, size_t nmemb, std::string *writer_data)
+static std::size_t curlcb_html(char *data, size_t size, size_t nmemb, std::string *writer_data)
 {
-	if(writer_data == nullptr) {
+	if (writer_data == nullptr) {
 		return 0;
 	}
 
-	writer_data->append(data, size*nmemb);
+	writer_data->append(data, size * nmemb);
 
 	return size * nmemb;
 }
 
-static bool init(CURL *&conn, char *url, std::string &buffer)
+static size_t curlcb_img(void *ptr, size_t size, size_t nmemb, void *userdata)
+{
+	FILE *stream = (FILE *) userdata;
+	if (!stream) {
+		std::printf("No file input stream\n");
+		return 0;
+	}
+
+	return (std::size_t) fwrite((FILE *) ptr, size, nmemb, stream);
+}
+
+bool download_img(char *url)
+{
+	auto fp = std::fopen("out.jpg", "wb");
+	if (!fp) {
+		std::printf("Failed to create file on the disk\n");
+		return false;
+	}
+
+	auto curl_ctx = curl_easy_init();
+
+	curl_easy_setopt(curl_ctx, CURLOPT_URL, url);
+	curl_easy_setopt(curl_ctx, CURLOPT_WRITEDATA, fp);
+	curl_easy_setopt(curl_ctx, CURLOPT_WRITEFUNCTION, curlcb_img);
+	curl_easy_setopt(curl_ctx, CURLOPT_FOLLOWLOCATION, 1);
+
+	CURLcode rc = curl_easy_perform(curl_ctx);
+	if (rc) {
+		std::printf("Failed to download: %s\n", url);
+		return false;
+	}
+
+	auto res_code = 0;
+	curl_easy_getinfo(curl_ctx, CURLINFO_RESPONSE_CODE, &res_code);
+
+	if (!((res_code == 200 || res_code == 201))) {
+		std::printf("Response code: %d\n", res_code);
+		return false;
+	}
+
+	curl_easy_cleanup(curl_ctx);
+	fclose(fp);
+
+	return true;
+}
+
+static bool init(CURL *& conn, char *url, std::string& buffer)
 {
 	CURLcode code;
 	char curl_error_buffer[CURL_ERROR_SIZE];
@@ -27,64 +73,38 @@ static bool init(CURL *&conn, char *url, std::string &buffer)
 		return false;
 	}
 
-	code = curl_easy_setopt(conn, CURLOPT_ERRORBUFFER, curl_error_buffer);
-	if (code != CURLE_OK) {
-		std::printf("Failed to set error buffer [%d]\n", code);
-		return false;
-	}
-
-	code = curl_easy_setopt(conn, CURLOPT_URL, url);
-	if (code != CURLE_OK) {
-		std::printf("Failed to set URL [%s]\n", curl_error_buffer);
-		return false;
-	}
-
-	code = curl_easy_setopt(conn, CURLOPT_FOLLOWLOCATION, 1L);
-	if (code != CURLE_OK) {
-		std::printf("Failed to set redirect option [%s]\n", curl_error_buffer);
-		return false;
-	}
-
-	code = curl_easy_setopt(conn, CURLOPT_WRITEFUNCTION, writer);
-	if (code != CURLE_OK) {
-		std::printf("Failed to set writer [%s]\n", curl_error_buffer);
-		return false;
-	}
-
-	code = curl_easy_setopt(conn, CURLOPT_WRITEDATA, &buffer);
-	if (code != CURLE_OK) {
-		std::printf("Failed to set write data [%s]\n", curl_error_buffer);
-		return false;
-	}
+	curl_easy_setopt(conn, CURLOPT_ERRORBUFFER, curl_error_buffer);
+	curl_easy_setopt(conn, CURLOPT_URL, url);
+	curl_easy_setopt(conn, CURLOPT_FOLLOWLOCATION, 1L);
+	curl_easy_setopt(conn, CURLOPT_WRITEFUNCTION, curlcb_html);
+	curl_easy_setopt(conn, CURLOPT_WRITEDATA, &buffer);
 
 	return true;
 }
 
-void traverse_dom_trees(xmlNode * a_node)
+void traverse_dom_trees(xmlNode *a_node)
 {
-    xmlNode *cur_node = nullptr;
+	xmlNode *cur_node = nullptr;
 
-    if(a_node == nullptr) {
-        return;
-    }
+	if (a_node == nullptr) {
+		return;
+	}
 
-    for (cur_node = a_node; cur_node; cur_node = cur_node->next) {
-		if (cur_node->type == XML_ELEMENT_NODE)	{
-			/* Check for if current node should be exclude or not */
-			printf("Node type: Text, name: %s\n", cur_node->name);
+	for (cur_node = a_node; cur_node; cur_node = cur_node->next) {
+		if (cur_node->type == XML_ELEMENT_NODE) {
+			std::printf("Node type: Text, name: %s\n", cur_node->name);
 		} else if (cur_node->type == XML_TEXT_NODE) {
-			/* Process here text node, It is available in cpStr :TODO: */
-			printf("node type: Text, node content: %s,  content length %d\n", (char *)cur_node->content, strlen((char *)cur_node->content));
+			std::printf("node type: Text, node content: %s,  content length %d\n", (char *) cur_node->content, strlen((char *) cur_node->content));
 		}
 
 		traverse_dom_trees(cur_node->children);
-    }
+	}
 }
 
 xmlNode *htmlparse_get_body(xmlNode *root_node)
 {
 	xmlNode *possible_body = root_node->children->next;
-	if (strncmp((char*)possible_body->name, "body", 5) == 0) {
+	if (strncmp((char *) possible_body->name, "body", 5) == 0) {
 		return possible_body;
 	}
 
@@ -124,7 +144,8 @@ int main(int argc, char **argv)
 	htmlDocPtr doc;
 	xmlNode *root_element = nullptr;
 
-	doc = htmlReadMemory(buffer.c_str(), buffer.size(), nullptr, nullptr, HTML_PARSE_NOBLANKS | HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING | HTML_PARSE_NONET);
+	doc = htmlReadMemory(buffer.c_str(), buffer.size(), nullptr, nullptr,
+						 HTML_PARSE_NOBLANKS | HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING | HTML_PARSE_NONET);
 	if (doc == nullptr) {
 		std::printf("Error parsing html.\n");
 		exit(EXIT_FAILURE);
@@ -136,7 +157,6 @@ int main(int argc, char **argv)
 		xmlFreeDoc(doc);
 		exit(EXIT_FAILURE);
 	}
-
 
 	xmlNode *html_body = htmlparse_get_body(root_element);
 	traverse_dom_trees(html_body);
