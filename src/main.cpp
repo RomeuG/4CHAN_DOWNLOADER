762bb54de5219cde6dba45899b83420fa883fe7c
Romeu Vieira <romeu.bizz@gmail.com>
2020-03-28 00:55:25 +0000

    get thread json

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,22 @@ auto get_catalog_json() -> std::string
     return result;
 }
 
+auto get_thread_json() -> std::string
+{
+    std::string result = "";
+
+    channer::get_thread_json(
+        "g", "",
+        [&result](std::string json) {
+            result = json;
+        },
+        [](std::string const& e) {
+            std::printf("Exception %s\n");
+        });
+
+    return result;
+}
+
 int main(int argc, char** argv)
 {
     int opt;
