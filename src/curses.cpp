2c8c791ab5ae9aea63262958065bcb453f93d2c9
Romeu Vieira <romeu.bizz@gmail.com>
2020-03-29 12:21:33 +0100

    successfuly moving cursor in ncurses

--- src/curses.cpp
+++ src/curses.cpp
@@ -3,8 +3,8 @@
 
 auto curses_catalog(Catalog catalog) -> void
 {
-    int width;
-    int height;
+    int winx;
+    int winy;
 
     WINDOW* window = 0;
 
@@ -13,13 +13,31 @@ auto curses_catalog(Catalog catalog) -> void
         return;
     }
 
-    getmaxyx(stdscr, height, width);
-    window = newwin(height - 2, width - 2, 1, 1);
+    noecho();
+    cbreak();
+    keypad(stdscr, 1);
+    nodelay(stdscr, 1);
+
+    getmaxyx(stdscr, winy, winx);
+    window = newwin(winy - 2, winx - 2, 1, 1);
 
     scrollok(window, 1);
 
+    // set cursor position
+    int curx = window->_curx;
+    int cury = window->_cury;
+
+    // main loop
     for (;;) {
-        wprintw(window, "Window dimensions: %d:%d\n", width, height);
+        //wprintw(window, "Window dimensions: %d:%d\n", winx, winy);
+        int ch;
+        if ((ch = getch()) != ERR) {
+            if (ch == KEY_DOWN) {
+                //wprintw(window, "KEY_DOWN = %d\n");
+                wmove(window, ++cury, curx);
+            }
+        }
+
         wrefresh(window);
     }
 
