29fc6f663bf9571c276ffc6aa40f1cbc0d84aa16
Romeu Vieira <romeu.bizz@gmail.com>
2021-09-12 22:53:08 +0100

    Parse html and show output as normal iamgeboard threads

--- FOURCHAN_DOWNLOADER/src/main.rs
+++ FOURCHAN_DOWNLOADER/src/main.rs
@@ -1,4 +1,61 @@
-use fourchan_sdk::core::repository::{self, get_catalog_json, get_thread_json};
+use fourchan_sdk::{
+    core::{
+        html::{html_parse_post, TextType},
+        repository::{self, get_catalog, get_catalog_json, get_thread, get_thread_json},
+    },
+    models::thread::Thread,
+};
+
+fn thread_to_str(thread: &Thread) -> String {
+    let mut result: String = String::new();
+
+    for post in &thread.posts {
+        // post name
+        if let Some(name) = &post.name {
+            let _name = format!("{} ", name);
+            result.push_str(_name.as_str());
+        }
+
+        // post tripcode
+        if let Some(tripcode) = &post.trip {
+            let _tripcode = format!("{} ", tripcode);
+            result.push_str(_tripcode.as_str());
+        }
+
+        // post file
+        // post date
+        // post number
+        let post_number = format!("no.{}\n", &post.no);
+        result.push_str(post_number.as_str());
+
+        if let Some(text) = &post.com {
+            let post_text = html_parse_post(&text);
+            for texttype in post_text {
+                match texttype {
+                    TextType::NewLine => {
+                        result.push_str("\n");
+                    }
+                    TextType::Link(t, _) => {
+                        result.push_str(&t);
+                    }
+                    TextType::Quote(t) => {
+                        result.push_str(&t);
+                    }
+                    TextType::Italics(t) => {
+                        result.push_str(&t);
+                    }
+                    TextType::PlainText(t) => {
+                        result.push_str(&t);
+                    }
+                }
+            }
+        }
+
+        result.push_str("\n\n");
+    }
+
+    return result;
+}
 
 fn main() {
     let args = clap::App::new("newsboat-archiver")
@@ -22,7 +79,7 @@ fn main() {
         .arg(
             clap::Arg::with_name("Board")
                 .short("b")
-                .long("directory")
+                .long("board")
                 .value_name("BOARD")
                 .help("4chan board")
                 .takes_value(true),
@@ -64,36 +121,27 @@ fn main() {
     let arg_file = args.value_of("File");
     let arg_images = args.value_of("Images");
 
-    println!("arg_has_catalog = {}", arg_has_catalog);
-    println!("arg_has_json = {}", arg_has_json);
-
     if arg_has_catalog {
+        let catalog = arg_catalog.unwrap();
+
         if arg_has_json {
-            let catalog = arg_catalog.unwrap();
             let result = get_catalog_json(catalog);
             println!("{}", result);
         } else {
+            let result = get_catalog(catalog);
+            // display as image board in a string
         }
     } else if arg_has_thread {
+        let board = arg_board.unwrap();
+        let thread = arg_thread.unwrap();
+
         if arg_has_json {
-            let board = arg_board.unwrap();
-            let thread = arg_thread.unwrap();
             let result = get_thread_json(board, thread);
             println!("{}", result);
         } else {
+            let result = get_thread(board, thread);
+            let thread_text = thread_to_str(&result);
+            println!("{}", thread_text);
         }
     }
-
-    // if let Some(c) = arg_catalog {
-    //     // get catalog
-    //     if arg_json {
-    //         get_thread_json(arg_board, thread)
-    //     } else {
-    //     }
-    // } else if let Some(t) = arg_thread {
-    //     // get thread
-    //     if arg_json {
-    //     } else {
-    //     }
-    // }
 }
